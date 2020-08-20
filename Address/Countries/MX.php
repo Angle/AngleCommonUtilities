f27d4e174f6aeacddb79ba6829efcc4b6da66137
Edmundo Fuentes <me@edmundofuentes.com>
2020-08-20 11:14:57 -0500

    Adding MX-CMX (CDMX) in Mexican States (regions)

--- Address/Countries/MX.php
+++ Address/Countries/MX.php
@@ -8,9 +8,16 @@ class MX extends Country
 {
     /**
      * @inheritDoc
+     *
+     * ISO 3166-2:MX is the entry for Mexico in ISO 3166-2, part of the ISO 3166 standard published by the International Organization for Standardization (ISO),
+     * which defines codes for the names of the principal subdivisions (e.g., provinces or states) of all countries coded in ISO 3166-1.
+     * Currently for Mexico, ISO 3166-2 codes are defined for 31 states and 1 capital. The capital of the country Mexico City has special status equal to the states.
+     * Each code consists of two parts, separated by a hyphen. The first part is MX, the ISO 3166-1 alpha-2 code of Mexico. The second part is three letters.
+     *
      * @var array
      */
     protected static $regions = array(
+        'MX-CMX' => 'Ciudad de México',
         'MX-AGU' => 'Aguascalientes',
         'MX-BCN' => 'Baja California',
         'MX-BCS' => 'Baja California Sur',
@@ -19,7 +26,7 @@ class MX extends Country
         'MX-CHH' => 'Chihuahua',
         'MX-COA' => 'Coahuila',
         'MX-COL' => 'Colima',
-        'MX-DIF' => 'Distrito Federal',
+        'MX-DIF' => 'Distrito Federal', // deprecated: substituted by MX-CMX
         'MX-DUR' => 'Durango',
         'MX-GUA' => 'Guanajuato',
         'MX-GRO' => 'Guerrero',
